99ee3aec3e5efbf4862af5727a10165459093801
Natali <nanatkim11@gmail.com>
2021-02-24 20:47:46 -0300

    [style] - fix maps's zoom

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -20,18 +20,16 @@ const Map: React.FC<Props> = ({ location }) => {
 
   async function getPosition() {
     const results = (await provider.search({ query: location })) as any;
-    console.log(results[0]);
 
     if (results[0] && results[0].x && results[0].y) {
       setLatLng([Number(results[0].y), Number(results[0].x)]);
-      console.log(latLng);
     }
   }
 
   return (
     <Container>
       {latLng.length > 0 && (
-        <MapContainer center={latLng} zoom={16} style={{ height: "200px" }}>
+        <MapContainer center={latLng} zoom={15} style={{ height: "200px" }}>
           <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
           <Marker position={latLng}>
             <Popup>{location}</Popup>
